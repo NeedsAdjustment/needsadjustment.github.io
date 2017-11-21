de0d5eba1393c73fb7d7cdba89863a49878ff6b3
sheepmolestor <edwardchen114@gmail.com>
2017-11-21 19:29:36 +1300

    game basically working but hardly any x-posts

---
+++ redditfeud/src/filter.js
@@ -0,0 +1,13 @@
+var IMAGE_DOMAINS = ["i.redd.it","i.imgur.com"];
+
+function filterDomains(listing) {
+  // only images allowed
+  var filtered = [];
+  for (i=0; i<listing.length; i++)
+    for (j=0; j<IMAGE_DOMAINS.length; j++)
+      if (listing[i].data.domain===IMAGE_DOMAINS[j]&&!listing[i].data.domain.endsWith(".gifv")&&!listing[i].data.domain.endsWith(".gif")) { // idk ok???
+        filtered.push(listing[i]);
+        continue;
+      }
+  return filtered;
+}
--- redditfeud/src/test.js
+++ redditfeud/src/test.js
@@ -10,3 +10,16 @@ function fetchContent(name) {
       return response.json();
     });
 }
+
+function fetchDuplicates(id) {
+  url = 'https://www.reddit.com/duplicates/';
+  url = url.concat(id,".json");
+  
+  return fetch(url)
+    .then(function (response) {
+      if (response.status >= 400) {
+        throw new Error('Bad response in fetchContent');
+      }
+      return response.json();
+    });
+}
