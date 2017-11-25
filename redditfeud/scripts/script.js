16e028fba340c77d40db5e18021b6e435805c3e6
sheepmolestor <edwardchen114@gmail.com>
2017-11-25 18:25:42 +1300

    submodule add

--- redditfeud/scripts/script.js
+++ redditfeud/scripts/script.js
@@ -5,15 +5,14 @@ function filterDomains(listing) {
   var filtered = [];
   for (i=0; i<listing.length; i++)
     for (j=0; j<IMAGE_DOMAINS.length; j++)
-      if (listing[i].data.domain===IMAGE_DOMAINS[j]&&!listing[i].data.domain.endsWith(".gifv")&&!listing[i].data.domain.endsWith(".gif")) { // idk ok???
+      if (listing[i].data.domain===IMAGE_DOMAINS[j]&&(listing[i].data.url.endsWith(".jpg")||listing[i].data.url.endsWith(".jpeg")||listing[i].data.url.endsWith(".png"))) { // idk ok???
         filtered.push(listing[i]);
         continue;
       }
   return filtered;
 }
 ;var items = [];
-var points = [];
-var guessed = [];
+var guessed = false;
 var score = 0;
 var TEST = [["a",1],["b",2],["c",3],["d",4],["e",5],["f",6],["g",7],["h",8]];
 var LISTING = {};
@@ -21,8 +20,9 @@ var DUPLICATE_LISTING = {};
 var IMAGE_ID = 0;
 
 // chosen from top posts in last month from these
-var SUBREDDITS = ["woahdude","aww","nevertellmetheodds","blackpeopletwitter","bikinibottomtwitter","mildlyinteresting","niceguys",
-  	"cringepics","facepalm","therewasanattempt","mildlyinfuriating","coaxedintoasnafu","madlads","crappydesign"];
+var SUBREDDITS = ["woahdude","aww","nevertellmetheodds","mildlyinteresting","iamverysmart","2meirl4meirl",
+  	"insanepeoplefacebook","therewasanattempt","mildlyinfuriating","coaxedintoasnafu","madlads","crappydesign","mypeopleneedme","thisismylifenow",
+    "wheredidthesodago","youdontsurf"];
 
 function init() {
   promise = fetchContent(SUBREDDITS[Math.floor(Math.random()*SUBREDDITS.length)]).then(function(value) {
@@ -37,11 +37,15 @@ function init() {
 
   Promise.all(huh).then(function() {
     LISTING = filterDomains(LISTING);
-    picked = Math.floor((Math.random() * LISTING.length));
-    pickedData = LISTING[picked].data;
-    document.getElementById("image").innerHTML = "<img src=\""+pickedData.url+"\" style=\"max-height:400px; max-width:auto;\"/>";
-    IMAGE_ID = pickedData.id;
-    setContent();
+    if (LISTING.length!=0) {
+      picked = Math.floor((Math.random() * LISTING.length));
+      pickedData = LISTING[picked].data;
+      document.getElementById("image").innerHTML = "<img src=\""+pickedData.url+"\" style=\"max-height:400px; max-width:auto;\"/>";
+      IMAGE_ID = pickedData.id;
+      setContent();
+    } else {
+      document.getElementById("image").innerHTML = "oops couldn't find anything";
+    }
   });
 }
 
@@ -57,15 +61,12 @@ function setContent() {
   huh.push(duplicatePromise);
 
   Promise.all(huh).then(function() {
-    answers = [];
+    items = [];
     for (i=0; i<DUPLICATE_LISTING.length; i++) {
-      items[i]=DUPLICATE_LISTING[i].data.subreddit;
-      points[i]=DUPLICATE_LISTING[i].data.score;
-      guessed[i] = false;
-      document.getElementById("item"+(i+1)).innerHTML = "?";
-    }
-    for (i=DUPLICATE_LISTING.length; i<8; i++) {
-      document.getElementById("item"+(i+1)).innerHTML = "_";
+      if (DUPLICATE_LISTING[i].data.score>100)
+        items.push(DUPLICATE_LISTING[i].data.subreddit);
+      guessed = false;
+      document.getElementById("answers").innerHTML = "???";
     }
   });
 }
@@ -79,11 +80,10 @@ function textSubmit(e) {
 function guess() {
   input = document.getElementById("guess").value;
   for (i=0; i<items.length; i++) {
-    if (items[i].toUpperCase()===input.toUpperCase() && !guessed[i]) {
-      document.getElementById("item"+(i+1)).innerHTML = items[i];
-      document.getElementById("points"+(i+1)).innerHTML = points[i];
-      guessed[i] = true;
-      score+=points[i];
+    if (items[i].toUpperCase()===input.toUpperCase() && !guessed) {
+      document.getElementById("answers").innerHTML = "/r/"+items[i];
+      guessed = true;
+      score++;
       document.getElementById("score").innerHTML = score;
     }
   }
