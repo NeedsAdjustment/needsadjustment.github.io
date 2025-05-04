c078a1acb4bdcf9d2b59e6e6964955ad73bd64c3
Joshua Soong <joshua@soong.nz>
2025-05-04 20:37:33 +1200

    basic scroll implementation (only for geniuses)

---
+++ src/scripts/scrolling.js
@@ -0,0 +1,26 @@
+import Lenis from 'lenis'
+
+window.scrollTo(0, 0)
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual'
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header')
+  const intro = document.querySelector('.intro')
+
+  const lenis = new Lenis({
+    autoRaf: true,
+  })
+  lenis.on('scroll', ({ direction }) => {
+    if (!header || !intro) return
+    if (direction > 0) {
+      header.style.transform = 'translateY(-50vh)'
+
+      intro.style.opacity = 1
+    } else {
+      header.style.transform = 'translateY(-50%)'
+      intro.style.opacity = 0
+    }
+  })
+})
