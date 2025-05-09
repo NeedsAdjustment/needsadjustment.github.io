1036e48e8f671590d4287c8120478f6bd0da7653
Joshua Soong <joshua@soong.nz>
2025-05-09 16:55:36 +1200

    added some scrolly scrolly

--- src/scripts/scrolling.js
+++ src/scripts/scrolling.js
@@ -8,6 +8,7 @@ if ('scrollRestoration' in history) {
 document.addEventListener('DOMContentLoaded', function () {
   const header = document.querySelector('header')
   const intro = document.querySelector('.intro')
+  const scrollIndicator = document.querySelector('.scroll-indicator')
 
   const lenis = new Lenis({
     autoRaf: true,
@@ -17,9 +18,11 @@ document.addEventListener('DOMContentLoaded', function () {
     if (direction > 0) {
       header.style.transform = 'translateY(-50vh)'
       intro.style.opacity = 1
+      scrollIndicator.style.opacity = 0
     } else {
       header.style.transform = 'translateY(-50%)'
       intro.style.opacity = 0
+      scrollIndicator.style.opacity = 1
     }
   })
 })
